bc0fddad97f0c4555ee3457dac5dd908b4f58db4
bash7325 <bash7325@gmail.com>
2022-04-22 15:55:53 -0600

    add scroll to top, needs icon centered

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+//scroll to top button
+$(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+$(document).ready(function () {
+    $("#toTop").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+})
