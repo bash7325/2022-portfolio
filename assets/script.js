8ca00ff6d67a0d9ddb5760bbd94048cad77b7b08
Brandon Ashby <bash7325@gmail.com>
2022-04-22 19:47:39 -0600

    added email JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+//AOS initialization
+AOS.init({
+    offset: 350,
+    duration: 1000,
+  });
 
 //scroll to top button
 $(window).scroll(function () {
@@ -16,10 +21,22 @@ $(document).ready(function () {
     });
 })
 
-//AOS initialization
-AOS.init({
-    offset: 350,
-    duration: 1000,
-  });
-
 //on mobile, hide text content of my projects and show the section below to keep the styling consistent
+
+//email functionality may need to add linebreak to the body/message
+$("#submit-button").on("click", function (event) {
+    event.preventDefault();
+    var userEmail = $("#user-email").val().trim();
+    var userMessage = $("#user-message").val().trim();
+    sendEmail();
+
+    function sendEmail() {
+      Email.send({
+        SecureToken: "4f7c11b9-5aee-4ecc-9ca6-7583e2fd8d74",
+        To: "bash7325@gmail.com",
+        From: "bash7325@gmail.com",
+        Subject: "Message from my portfolio",
+        Body: userMessage + "\n" + userEmail,
+      }).then((message) => $("#submit-button").text("Message Sent"));
+    }
+  });
